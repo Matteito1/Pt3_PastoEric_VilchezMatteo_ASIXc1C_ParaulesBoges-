0be9d8e0be7b043d37979c34c1251c93055ba6f0
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-04-30 11:39:59 +0200

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,17 @@
+"""
+    Eric Pasto i Matteo Vilchez.
+    30/04/2024
+    ASIXc 1B  M03 UF3 A1
+    ParaulesBoges Pt.3
+    Implementa un arxiu de entrada, log i un arxiu de sortida tot senza menu
+"""
+FILE_NAME = "paraules.txt"
+import random
+cEspeciales = [".", ",", "?", ":", ";", "!", "'", "¡", "¿"]
+Texto_Ordenado=[]
+palabras=[]
+frase_Desordenado=[]
+
 def leerParaules():
     with open(FILE_NAME, mode='r', encoding='UTF-8') as fichero:
         contenido = fichero.read()
