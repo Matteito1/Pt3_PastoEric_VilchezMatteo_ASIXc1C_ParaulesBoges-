c323c1ef5f114bee08f02a3470012dd6da85ceaf
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-04-30 11:49:25 +0200

    Update ejemplo.py

--- ejemplo.py
+++ ejemplo.py
@@ -5,16 +5,44 @@
     ParaulesBoges Pt.3
     Implementa un arxiu de entrada, log i un arxiu de sortida tot senza menu
 """
-FILE_NAME = "paraules.txt"
 import random
+
+FILE_NAME = "paraules.txt"
 cEspeciales = [".", ",", "?", ":", ";", "!", "'", "¡", "¿"]
-Texto_Ordenado=[]
-palabras=[]
-frase_Desordenado=[]
+Texto_Ordenado = []
+palabras = []
+frase_Desordenado = []
 
-def leerParaules():
-    with open(FILE_NAME, mode='r', encoding='UTF-8') as fichero:
-        contenido = fichero.read()
-        print(contenido)
+def leerParaules(FILE_NAME):
+    with open(FILE_NAME, 'r') as f:
+        f = f.read()
+        return f
 
-leerParaules()
+def separar(leerParaules):
+    for palabrades in leerParaules.split():
+        if len(palabrades) >= 3:
+            if palabrades[-1] in cEspeciales:
+                sep = list(palabrades[1:-1])
+                random.shuffle(sep)
+                separado = palabrades[0] + ''.join(sep) + palabrades[-1]
+            else:
+                sep = list(palabrades[1:-2])
+                random.shuffle(sep)
+                separado = palabrades[0] + ''.join(sep) + palabrades[-2] + palabrades[-1]
+        else:
+            separado = palabrades
+        palabras.append(separado)
+    return palabras
+
+def juntar():
+    frasedes = ' '.join(palabras)
+    return frasedes
+
+def resultado():
+    print("Texto desordenado:")
+    print(frase_Desordenado)
+
+texto_leido = leerParaules(FILE_NAME)
+palabras = separar(texto_leido)
+frase_Desordenado = juntar()
+resultado()
