38db4df4039ff95df05b8f9f3cd00c0b5457063b
Matteito1 <124351564+Matteito1@users.noreply.github.com>
2024-05-02 09:50:27 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+    Eric Pasto i Matteo Vilchez.
+    30/04/2024
+    ASIXc 1B  M03 UF3 A1
+    ParaulesBoges Pt.3
+    Implementa un arxiu de entrada, log i un arxiu de sortida tot sense menu
+"""
+
+import paraulesboges
+
+paraulesboges.procesar_archivos()
